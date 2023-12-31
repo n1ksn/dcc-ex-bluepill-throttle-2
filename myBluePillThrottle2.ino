6598aa65af943ac397dbf814e68f6fefafdfc178
n1ksn <57112752+n1ksn@users.noreply.github.com>
2023-12-31 15:17:19 -0600

    Update myBluePillThrottle2.ino

--- myBluePillThrottle2.ino
+++ myBluePillThrottle2.ino
@@ -94,6 +94,7 @@ int fnOnFlags[10];  // On/off = 1/0 states of functions F0 to F9
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+// Contrary to specs, I had to use 0x3C screen address below for both
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
